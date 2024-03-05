00c04135e15d2291a2e5d9a1ef40fc8acc4107e8
christinaRas <162343759+setrakely@users.noreply.github.com>
2024-03-05 18:39:19 +0300

    update

--- demo/src/main/java/com/example/demo/repositories/CultureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domaine.Culture;
-
-public interface CultureRepository extends JpaRepository<Culture, Integer> {
-    // You can add custom query methods here if needed
-}
--- demo/src/main/java/com/example/demo/repositories/ParcelleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domaine.Parcelle;
-
-public interface ParcelleRepository extends JpaRepository<Parcelle, Integer> {
-    // You can add custom query methods here if needed
-}
